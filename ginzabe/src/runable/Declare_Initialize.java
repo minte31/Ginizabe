d4768a346f4b39d3016e832d64039adf959b4e85
Mintesnot Yirga <mintesyirga@gmail.com>
2024-04-30 08:53:11 -0400

    Update Ginzabe

---
+++ ginzabe/src/runable/Declare_Initialize.java
@@ -0,0 +1,19 @@
+package runable;
+
+public class Declare_Initialize {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Declare (Data type>>Variable ) -> we don't include value
+		String myString;
+		int myInt;
+		double myDouble;
+		
+		// Initialized value(Variable >> Value)
+		myString = "Ginzabe";
+		myInt = 8;
+		myDouble = 8.0;
+		
+	}
+
+}
---
+++ ginzabe/src/runable/GinzabeAdventure.java
@@ -0,0 +1,16 @@
+package runable;
+
+public class GinzabeAdventure {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+	 String GT1;
+     String GT2;
+     
+     GT1 = "Mintesnot";
+     GT2 = "Tensaye";
+     System.out.println(GT1 + " "+GT2);
+	}
+
+}
