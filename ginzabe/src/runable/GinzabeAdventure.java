fec14ede500b183ff445e0e313e2f68096c72c1d
Mintesnot Yirga <mintesyirga@gmail.com>
2024-04-30 09:03:31 -0400

    Update Ginzabe

--- ginzabe/src/runable/GinzabeAdventure.java
+++ ginzabe/src/runable/GinzabeAdventure.java
@@ -4,13 +4,18 @@ public class GinzabeAdventure {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     
+     //Declare
+		
 	 String GT1;
      String GT2;
+     String GT3;
      
+     // Initialize
      GT1 = "Mintesnot";
      GT2 = "Tensaye";
-     System.out.println(GT1 + " "+GT2);
+     GT3 = GT1 + " " + GT2;
+     
+     System.out.println(GT3);
 	}
 
 }
